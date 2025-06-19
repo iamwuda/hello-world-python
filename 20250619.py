b7144616f82f0e20daac50f601fcc6c0ff9a1555
iamwuda <794373522@qq.com>
2025-06-19 19:31:01 +0800

    更新Hello World程序

--- 20250619.py
+++ 20250619.py
@@ -7,3 +7,20 @@ if __name__ == "__main__":
 
 def test_main():
     assert main() == "Hello, World!"
+
+
+print(main())
+
+test_main()
+
+
+def test_1():
+    assert 1 == 1
+
+def test_2():
+    assert 2 == 2
+
+def test_3():
+    assert 3 == 3
+
+def test_4():
