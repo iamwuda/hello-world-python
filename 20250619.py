c89c0ccb5965e60034095999a80b4a6843eb21ee
iamwuda <794373522@qq.com>
2025-06-19 19:44:02 +0800

    移除测试函数和打印语句，简化Hello World程序

--- 20250619.py
+++ 20250619.py
@@ -3,24 +3,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-
-def test_main():
-    assert main() == "Hello, World!"
-
-
-print(main())
-
-test_main()
-
-
-def test_1():
-    assert 1 == 1
-
-def test_2():
-    assert 2 == 2
-
-def test_3():
-    assert 3 == 3
-
-def test_4():
