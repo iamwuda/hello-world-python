ee9786b5c721a6db162f50d9f63d281bdb61df84
iamwuda <794373522@qq.com>
2025-06-20 10:50:17 +0800

    20250620新提交

--- 20250619.py
+++ 20250619.py
@@ -4,24 +4,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# TabError: inconsistent use of tabs and spaces in indentation
-
-
-
-
-
-
-
-
-main()
-
-
-try:
-    print(10/0)
-except ZeroDivisionError:
-    print("除数不能为0")
-
-
-最后一次测试
-
 
